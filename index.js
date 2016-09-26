abfb2631d319887c3a690c53ef8b15b99359fa56
z1z2 <zhang1zhe2@gmail.com>
2016-09-26 15:40:37 +0800

    use symbolab to check the answer

--- index.js
+++ index.js
@@ -1,90 +1,99 @@
-  var prompt = require('prompt');
-
+var prompt = require('prompt');
+var readlineSync = require('readline-sync');
+var cp = require('child_process');
 var variables = 5
 var polys = 5
 var steps = 5
 
-  var properties = [
+var properties = [
     {
-      name: 'variables', 
-      validator: /^[1-9]+$/,
-      warning: 'variables must be from 1 to 9'
+        name: 'variables',
+        validator: /^[1-9]{1}?$/,
+        warning: 'variables must be from 1 to 9'
     },
     {
-      name: 'polys', 
-      validator: /^[2-5]+$/,
-      warning: 'polys must be from 2 to 5'
+        name: 'polys',
+        validator: /^[2-5]{1}?$/,
+        warning: 'polys must be from 2 to 5'
     },
     {
-      name: 'steps', 
-      validator: /^[2-4]+$/,
-      warning: 'steps must be from 2 to 4'
+        name: 'steps',
+        validator: /^[2-5]{1}?$/,
+        warning: 'steps must be from 2 to 5'
     },
     {
-      name: 'questions', 
-      validator: /^[1-9]+$/,
-      warning: 'questions must be from 1 to 9'
+        name: 'questions',
+        validator: /^[1-9]{1}?$/,
+        warning: 'questions must be from 1 to 9'
     },
   ];
 
-  prompt.start();
+prompt.start();
 
-  prompt.get(properties, function (err, result) {
+prompt.get(properties, function(err, result) {
     if (err) { return onErr(err); }
-    
-    if(variables > result.variables)
+
+    if (variables > result.variables)
         variables = result.variables;
-    if(polys > result.polys)
+    if (polys > result.polys)
         polys = result.polys;
-    if(steps > result.steps)
+    if (steps > result.steps)
         steps = result.steps;
 
-    for(var q=0; q<result.questions; q++)
-        console.log(
-            getExpr(steps)
+    for (var q = 0; q < result.questions; q++) {
+
+        var expr = getExpr(steps);
+        console.log('\n\n' + (q + 1) + '] ' +
+            expr
         );
 
-        console.log('\r\n');
-  });
+        expr = expr.replace(/\+/g, '%2B');
+        expr = expr.replace(/\(/g, '%5Cleft(');
+        expr = expr.replace(/\)/g, '%5Cright)');
 
-  function onErr(err) {
+        readlineSync.question('Press [Enter] to check the answer!');
+
+        cp.exec('start https://www.symbolab.com/solver/polynomial-calculator/' + expr);
+    }
+
+});
+
+function onErr(err) {
     console.log(err);
     return 1;
-  }
-
-
-
-var vars='abcdefghijkl';
-
-
-function getSign(){
-    return Math.random()> .5 ? '+':'-';
 }
 
-function getCoeff(){
-    var c = Math.round(Math.random()*10+1);
+
+
+var vars = 'abcdefghijkl';
+
+
+function getSign() {
+    return Math.random() > .5 ? '+' : '-';
+}
+
+function getCoeff() {
+    var c = Math.round(Math.random() * 10 + 1);
     if (c == 1)
         return '';
     else
-        return c;    
+        return c;
 }
 
-function getExpr(step){
-    if(step == 1)
-        return vars[(Math.round(Math.random()*10) % variables )]
-    else
-    {
-        var exp = step==steps?'':'(';
-        for (var i = 0; i< polys; i++)
-        {
+function getExpr(step) {
+    if (step == 1)
+        return vars[(Math.round(Math.random() * 10) % variables)]
+    else {
+        var exp = step == steps ? '' : '(';
+        for (var i = 0; i < polys; i++) {
             var sign = getSign();
-            if(sign=='+' && i==0)
+            if (sign == '+' && i == 0)
                 sign = '';
             exp += (
-                sign + getCoeff() + getExpr(step-1)
+                sign + getCoeff() + getExpr(step - 1)
             );
         }
-        exp += step==steps?'':')';
+        exp += step == steps ? '' : ')';
         return exp;
     }
 }
