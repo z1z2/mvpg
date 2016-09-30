889dfa7546e94a0d75fa867a634498ca3972f8a4
z1z2 <zhang1zhe2@gmail.com>
2016-09-30 17:31:01 +0800

    add equation generator

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var prompt = require('prompt');
 prompt.message = '';
-prompt.delimiter = '><'
+prompt.delimiter = '>'
 var readlineSync = require('readline-sync');
 var cp = require('child_process');
 var nestings = 3
 var degrees = 1
 var properties = [
     {
+        name: 'type',
+        message: 'polynomial [1] or equation [2]',
+        validator: /^[1-2]{1}$/,
+        default: 1,
+        warning: 'input must be 1 or 2'
+    },
+    {
         name: 'degrees',
+        message: 'linear [1] or quadratic [2]',
         validator: /^[1-2]{1}$/,
         default: 1,
         warning: 'degrees must be from 1 to 2'
@@ -27,32 +35,11 @@ prompt.get(properties, function (err, result) {
     degrees = result.degrees;
 
     for (var q = 0; q < result.questions; q++) {
-
-        var expr = '';
-        if (degrees == 1)
-            expr = getLinear(nestings);
+        console.log('\n\n' + (q + 1) + '] ');
+        if (result.type == 1)
+            generatePolynomial();
         else
-            expr = getHighDegreePoly(degrees);
-        console.log('\n\n' + (q + 1) + '] ' +
-            expr
-        );
-
-        expr = expr.replace(/\+/g, '%2B');
-        expr = expr.replace(/\(/g, '%5Cleft(');
-        expr = expr.replace(/\)/g, '%5Cright)');
-
-        readlineSync.question('Press [Enter] to check the answer!');
-
-        if (require('os').type() == 'Windows_NT')
-            cp.exec('start https://www.symbolab.com/solver/polynomial-calculator/' + expr);
-        else if (require('os').type() == 'Darwin')
-            cp.exec('open "https://www.symbolab.com/solver/polynomial-calculator/' + expr + '"', (err, stdout, stderr) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                console.log(stdout);
-            });
+            generateEquation();
     }
 });
 
@@ -65,6 +52,10 @@ var vars = 'abcdefghijkl';
 
 function getSign() {
     return Math.random() > .5 ? '+' : '-';
+}
+
+function getConstant() {
+    return Math.round(Math.random() * 10 + 1);
 }
 
 function getCoeff() {
@@ -99,4 +90,72 @@ function getHighDegreePoly(degree) {
     for (var i = 0; i < degree; i++)
         ret += getLinear(2);
     return ret;
+}
+
+function generatePolynomial() {
+
+    var expr = '';
+    if (degrees == 1)
+        expr = getLinear(nestings);
+    else
+        expr = getHighDegreePoly(degrees);
+    console.log(expr);
+
+    check(expr);
+}
+
+function getEquationPart(degree) {
+    if (degree == 0)
+        return getCoeff();
+    else {
+        return '(' + getCoeff() + 'x' + getSign() + getConstant() + ')';
+    }
+}
+
+function generateEquation() {
+    var expr = '';
+    var simple = Math.random() < .5;
+    var sign = getSign();
+    if (sign == '+')
+        sign = '';
+    expr += sign;
+    if (degrees == 1)
+        expr += getEquationPart(0) + getEquationPart(1);
+    else
+        expr += getEquationPart(1) + getEquationPart(1);
+
+    if (!simple) {
+        expr += getSign();
+        if (degrees == 1)
+            expr += getEquationPart(0) + getEquationPart(1);
+        else
+            expr += getEquationPart(1) + getEquationPart(1);
+    }
+    sign = getSign();
+    if (sign == '+')
+        sign = '';
+    expr += '=' + sign + getCoeff() + getEquationPart(1);
+    console.log(expr);
+
+    check(expr);
+}
+
+function check(expr) {
+
+    expr = expr.replace(/\+/g, '%2B');
+    expr = expr.replace(/\(/g, '%5Cleft(');
+    expr = expr.replace(/\)/g, '%5Cright)');
+
+    readlineSync.question('Press [Enter] to check the answer!');
+
+    if (require('os').type() == 'Windows_NT')
+        cp.exec('start https://www.symbolab.com/solver/polynomial-calculator/' + expr);
+    else if (require('os').type() == 'Darwin')
+        cp.exec('open "https://www.symbolab.com/solver/polynomial-calculator/' + expr + '"', (err, stdout, stderr) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(stdout);
+        });
 }
